81195f0ea563e6c2a1913b57e94487b80fcb7db5
Robin Luley <robin.luley@qbeyond.de>
2022-10-04 15:12:03 +0200

    feat(vite-template): small routing adjustments

--- template-vite/src/RootRouter.tsx
+++ template-vite/src/RootRouter.tsx
@@ -1,11 +1,12 @@
-import { StartPage } from 'pages/StartPage';
 import { FunctionComponent } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFoundPage, StartPage } from 'pages';
 
 export const RootRouter: FunctionComponent = () => (
   <BrowserRouter>
     <Routes>
       <Route path={`${StartPage.path}*`} element={<StartPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
