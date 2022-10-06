928220fc1eb38c75e700456a76fe31ef7be23192
Robin Luley <robin.luley@qbeyond.de>
2022-10-06 12:16:54 +0200

    feat(vite-template): load fallbackLng from env

--- template-vite/src/locales/index.ts
+++ template-vite/src/locales/index.ts
@@ -1,5 +1,6 @@
 import { de } from './de';
 import { en } from './en';
+import { isSupportedLanguage } from 'utils/locales';
 
 // the resource definitions used by i18next
 export const resources = { en, de } as const;
@@ -10,9 +11,13 @@ export type TranslationResource = typeof en;
 export type TranslationKey = keyof TranslationResource;
 
 // App wide locale configuration options
-export const fallbackLng: LanguageCode = 'en';
-export const defaultLng: LanguageCode = 'en';
 export const supportedLngs = Object.keys(resources) as readonly LanguageCode[];
+
+// Set fallback language from environment
+const envFallbackLng = import.meta.env.VITE_FALLBACK_LANGUAGE as string;
+export const fallbackLng: LanguageCode = isSupportedLanguage(envFallbackLng)
+  ? envFallbackLng
+  : supportedLngs[0];
 export const defaultNS: TranslationKey = 'common';
 
 // a set of BCP 47 language codes
--- template-vite/src/utils/locales.ts
+++ template-vite/src/utils/locales.ts
@@ -7,19 +7,23 @@ import {
 } from 'locales';
 
 /**
- * Checks if a given language code is supported
+ * Type guard, that checks if a given language code is supported
  * @param languageCode the language code string to check
  * @returns boolean
  */
-export const isSupportedLanguage = (languageCode: string) =>
+export const isSupportedLanguage = (
+  languageCode: string,
+): languageCode is LanguageCode =>
   supportedLngs.includes(languageCode as LanguageCode);
 
 /**
- * Checks if a given culture code is supported
+ * Type guard, that checks if a given culture code is supported
  * @param locale the culture code string to check
  * @returns boolean
  */
-export const isSupportedCultureCode = (cultureCode: string) =>
+export const isSupportedCultureCode = (
+  cultureCode: string,
+): cultureCode is CultureCode =>
   cultureCodes.includes(cultureCode as CultureCode);
 
 /**
