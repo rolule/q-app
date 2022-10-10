dea814a22b364c18341f2a274dd912828cb0ba4e
Robin Luley <robin.luley@qbeyond.de>
2022-10-10 09:57:26 +0200

    docs(vite-template): improve documentation

--- template-vite/src/components/form/FormInput.tsx
+++ template-vite/src/components/form/FormInput.tsx
@@ -1,30 +1,51 @@
-import { Input, InputProps } from '@chakra-ui/react';
-import { FunctionComponent } from 'react';
 import {
-  Controller,
-  ControllerProps,
-  RegisterOptions,
-  useFormContext,
-} from 'react-hook-form';
+  FormControl,
+  FormControlProps,
+  FormErrorMessage,
+  Input,
+  InputProps,
+} from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+import { Controller, ControllerProps, useFormContext } from 'react-hook-form';
 
 export type FormInputProps = {
   name: string;
-  rules?: RegisterOptions;
+  showError?: boolean;
+  controlProps?: FormControlProps;
 } & InputProps;
 
+/**
+ * A smart form Input, that automatically registers itself to a parent SmartForm under the given name
+ * @param name the name of the form input
+ * @param showError enables showing the error message when the input is invalid
+ * @param controlProps allows passing additional parameters to the FormControl element
+ */
 export const FormInput: FunctionComponent<FormInputProps> = ({
   name,
-  rules,
+  showError = false,
+  controlProps,
   ...inputProps
 }) => {
-  const { control } = useFormContext();
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  const errorMessage = errors[name]?.message as string;
 
   const controllerProps: ControllerProps = {
     control,
     name,
-    rules,
 
-    render: ({ field }) => <Input {...field} {...inputProps} />,
+    render: ({ field }) => (
+      <FormControl isInvalid={!!errors[name]} {...controlProps}>
+        <Input {...field} {...inputProps} />
+
+        {showError && errorMessage && (
+          <FormErrorMessage>{errorMessage}</FormErrorMessage>
+        )}
+      </FormControl>
+    ),
   };
 
   return <Controller {...controllerProps} />;
